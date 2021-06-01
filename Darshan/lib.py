fc7904f4cbce9ddbd0740bb8613b4cd632a00e13
SINGHdarshanS <dssinghm699@gmail.com>
2021-06-01 16:14:26 -0700

    testing functionality left to implement

--- Darshan/lib.py
+++ Darshan/lib.py
@@ -1,2 +1,34 @@
+from random import random, seed
+import numpy as np
+
+
+def init_shuffle(test=False):
+    seed()
+    return test
+
+
 def gen_playlist(n):
     return list(range(n))
+
+
+def gen_placements(size):
+    shufflelist = list(np.zeros((size,), dtype=int))
+    i = 0
+    while i < size:
+        shufflelist[i] = random() - 2
+        i += 1
+    for place in range(size):
+        index = shufflelist.index(min(shufflelist))
+        shufflelist[index] = place
+    return shufflelist
+
+
+def rearrange(target, placements):
+    retlist = list(np.zeros((len(target),), dtype=int))
+    for i in range(len(target)):
+        retlist[i] = target[placements.index(i)]
+    return retlist
+
+
+def test():
+    pass
--- Darshan/main.py
+++ Darshan/main.py
@@ -1,4 +1,19 @@
 from lib import *
-# if __name__ == '__main__':
+
+gate = init_shuffle(test=True)
+
+# generating a playlist of length n #####################
 print('shuffling...')
-playlist = gen_playlist(5)
+playlist = gen_playlist(20)
+print("Original playlist: {}".format(playlist))
+
+# generating a list of randoms of equal size to playlist and using that to order the new list
+shufflelist = gen_placements(len(playlist))
+
+# rearranging playlist based on order specified by shufflelist
+playlist = rearrange(playlist, shufflelist)
+print("Shuffled playlist: {}".format(playlist))
+
+# run optional tests here if gate boolean is set to True during init at start of script
+if gate: # run testing spectrum here
+    pass
